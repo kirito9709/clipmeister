e67b690244df01c17bb25b842ab7c5b275e4db6c
musashi <mus1412kid1@gmail.com>
2026-03-09 16:24:54 +0100

    Create Input component with Tailwind styling

---
+++ src/components/Input.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+interface InputProps {
+    type?: string;
+    placeholder?: string;
+    value: string;
+    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+const Input: React.FC<InputProps> = ({ type = 'text', placeholder, value, onChange }) => {
+    return (
+        <input
+            type={type}
+            placeholder={placeholder}
+            value={value}
+            onChange={onChange}
+            className={`
+                w-full p-2 border border-gray-300 rounded
+                focus:outline-none focus:ring-2 focus:ring-blue-500
+            `}
+        />
+    );
+};
+
+export default Input;
